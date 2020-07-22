b22bb70e8f0b912239183905367685ceead629e1
Marc_M <marcmartinez987@gmail.com>
2020-07-21 20:45:54 -0700

    connections added

---
+++ config/connection.js
@@ -0,0 +1,31 @@
+var mysql = require("mysql");
+
+//change connection to include JAWSDB
+var connection;
+if (process.env.JAWSDB_URL) {
+    // Database is JawsDB on Heroku
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    // Database is local
+    connection = mysql.createConnection({
+        port: 3306,
+        host: 'localhost',
+        user: 'root',
+        password: 'Marc987987!!12',
+        database: 'weight_db'
+    })
+};
+
+//Make connection.
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
+
+connection.connect();
+
+// Export connection for our ORM to use.
+module.exports = connection;
