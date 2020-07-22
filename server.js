1e771027c4c7ad84dfaa0473833c2213b2347747
Marc_M <marcmartinez987@gmail.com>
2020-07-21 19:29:32 -0700

    Thank you Raffa!

--- server.js
+++ server.js
@@ -1,47 +1,12 @@
-require("dotenv").config();
-var express = require("express");
-var exphbs = require("express-handlebars");
+let PORT = process.env.PORT || 3000;
+let express = require("express");
+let app = express();
 
-var db = require("./models");
+app.engine('handlebars', exphbs({defaultLayout: 'index.handlebars'}));
+app.set('view engine', 'handlebars');
 
-var app = express();
-var PORT = process.env.PORT || 3000;
-
-// Middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use(express.static("public"));
-
-// Handlebars
-app.engine(
-  "handlebars",
-  exphbs({
-    defaultLayout: "main"
-  })
-);
-app.set("view engine", "handlebars");
-
-// Routes
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
-
-var syncOptions = { force: false };
-
-// If running a test, set syncOptions.force to true
-// clearing the `testdb`
-if (process.env.NODE_ENV === "test") {
-  syncOptions.force = true;
-}
-
-// Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function() {
-  app.listen(PORT, function() {
-    console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
-  });
+app.get('/', function(error, data){
+    data.render('index.handlebars');
 });
 
-module.exports = app;
+app.listen(PORT);
