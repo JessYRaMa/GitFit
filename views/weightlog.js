ffb37ea80e199759f0c63398aabb68a472c9084a
Yssabel Pangilinan <yssabelpangilinan@yahoo.com>
2020-07-25 10:56:29 -0700

    additing create account user to db

--- views/weightlog.js
+++ views/weightlog.js
@@ -16,9 +16,11 @@ function getPostData() {
             if(users.indexOf(data[i].username) === -1){
                 users.push(data[i].username);
             }
+
+            initializeRows(); 
         };
         myChart.update();
-       UserDropdown();    
+       UserDropdown();   
     });
   };
 
@@ -187,6 +189,88 @@ function deletePost(id) {
       .then(function() {
         myChart.update();
       });
+  }
+
+  var blogContainer = $(".allLogs");
+
+  function initializeRows() {
+    blogContainer.empty();
+    var postsToAdd = [];
+    var post = data;
+    for (var i = 0; i < post.length; i++) {
+      postsToAdd.push(createNewRow(post[i].weight));
+      postsToAdd.push(createNewRow(post[i].logged_at));
+      postsToAdd.push(createNewRow(post[i].username));
+    }
+    blogContainer.append(postsToAdd);
+  }
+
+  function createNewRow(post) {
+    var newPostCard = $("<div>");
+    newPostCard.addClass("card");
+    var newPostCardHeading = $("<div>");
+    newPostCardHeading.addClass("card-header");
+    var deleteBtn = $("<button>");
+    deleteBtn.text("x");
+    deleteBtn.addClass("delete btn btn-danger");
+    var editBtn = $("<button>");
+    editBtn.text("EDIT");
+    editBtn.addClass("edit btn btn-default");
+    var newPostTitle = $("<h2>");
+    var newPostDate = $("<small>");
+    var newPostCategory = $("<h5>");
+    newPostCategory.text(post.category);
+    newPostCategory.css({
+      float: "right",
+      "font-weight": "700",
+      "margin-top":
+      "-15px"
+    });
+    var newPostCardBody = $("<div>");
+    newPostCardBody.addClass("card-body");
+    var newPostBody = $("<p>");
+    newPostTitle.text(post.title + " ");
+    newPostBody.text(post.body);
+    var formattedDate = new Date(post.createdAt);
+    formattedDate = moment(formattedDate).format("MMMM Do YYYY, h:mm:ss a");
+    newPostDate.text(formattedDate);
+    newPostTitle.append(newPostDate);
+    newPostCardHeading.append(deleteBtn);
+    newPostCardHeading.append(editBtn);
+    newPostCardHeading.append(newPostTitle);
+    newPostCardHeading.append(newPostCategory);
+    newPostCardBody.append(newPostBody);
+    newPostCard.append(newPostCardHeading);
+    newPostCard.append(newPostCardBody);
+    newPostCard.data("post", post);
+    return newPostCard;
+  }
+
+  // This function figures out which post we want to delete and then calls
+  // deletePost
+  function handlePostDelete() {
+    var currentPost = $(this)
+      .parent()
+      .parent()
+      .data("post");
+    deletePost(currentPost.id);
+  }
+
+  // This function figures out which post we want to edit and takes it to the
+  // Appropriate url
+  function handlePostEdit() {
+    var currentPost = $(this)
+      .parent()
+      .parent()
+      .data("post");
+    window.location.href = "/api/weight/id=" + currentPost.id;
+  }
+  function displayEmpty() {
+    blogContainer.empty();
+    var messageH2 = $("<h2>");
+    messageH2.css({ "text-align": "center", "margin-top": "50px" });
+    messageH2.html("No posts yet for this category, navigate <a href='/cms'>here</a> in order to create a new post.");
+    blogContainer.append(messageH2);
   }
 
 
