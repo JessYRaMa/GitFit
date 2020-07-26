d5317596dd676340291c57bbb4a184269e0607e0
Raffaj1208 <61484778+Raffaj1208@users.noreply.github.com>
2020-07-25 21:47:41 -0700

    restructured scopes

--- views/bmiCalc.js
+++ views/bmiCalc.js
@@ -1,25 +1,22 @@
+var units = $('#units').val();
+var weightType = $('#weightType').val();
+var heightType = $('#heightType').val();
+var weight = $("#weight").val().trim();
+var height = $("#height").val().trim();
 
     $('#calculate').on('click', function(){
-        let units = $('#units').val();
-        let weightType = $('#weightType').val();
-        let heightType = $('#heightType').val();
-        let weight = $("#weight").val().trim();
-        let height = $("#height").val().trim();
-    
-    
-    function Bmi() {
-        let imperialBmi = function(){
-        let impBmi = weight / height * 703;
         if (units == 'Imperial' && weightType == 'Lbs' && heightType == 'Inches') {
-            imperialBmi();
+            $('#yourBmi').html(imperialBmi());
         }
+        if (units == 'Metric' && weightType == 'Kg' && heightType == 'Cm') {
+            $('#yourBmi').html(metricBmi());
+            }
+    });
+    
+    var imperialBmi = function(){
+       return weight / height * 703;
     };
-    let metricBmi = function(){
-        let metBmi = weight / height;
-        if(units == 'Metric' && weightType == 'Kg' && heightType == 'Cm') {
-            metricBmi();
-        }
-    };
+    
+    var metricBmi = function(){
+     return weight / height;
 };
-Bmi();
-});
