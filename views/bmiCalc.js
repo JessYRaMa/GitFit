62c2150fa1126523314290be5791bc313e35d753
Raffaj1208 <61484778+Raffaj1208@users.noreply.github.com>
2020-07-25 21:24:34 -0700

    bmi scoping

--- views/bmiCalc.js
+++ views/bmiCalc.js
@@ -7,18 +7,19 @@
         let height = $("#height").val().trim();
     
     
-    let imperialBmi = function(){
+    function Bmi() {
+        let imperialBmi = function(){
         let impBmi = weight / height * 703;
         if (units == 'Imperial' && weightType == 'Lbs' && heightType == 'Inches') {
             imperialBmi();
-        };
-        imperialBmi();
-            }
+        }
+    };
     let metricBmi = function(){
         let metBmi = weight / height;
         if(units == 'Metric' && weightType == 'Kg' && heightType == 'Cm') {
             metricBmi();
         }
     };
-    metricBmi();
+};
+Bmi();
 });
