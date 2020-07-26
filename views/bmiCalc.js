4653cdf67475896094cd690ecf54925a60ddd6f7
Raffaj1208 <61484778+Raffaj1208@users.noreply.github.com>
2020-07-25 21:11:55 -0700

    restructured if parameter statement

--- views/bmiCalc.js
+++ views/bmiCalc.js
@@ -1,24 +1,22 @@
 
     $('#calculate').on('click', function(){
-    let units = $('#units').val();
-    let weightType = $('#weightType').val();
-    let heightType = $('#heightType').val();
-    let weight = $("#weight").val().trim();
-    let height = $("#height").val().trim();
-    let impBmi;
-    let metBmi;
-    console.log(impBmi);
+        let units = $('#units').val();
+        let weightType = $('#weightType').val();
+        let heightType = $('#heightType').val();
+        let weight = $("#weight").val().trim();
+        let height = $("#height").val().trim();
+    
     
     let imperialBmi = function(){
-        impBmi = weight / height * 703;
-        if ( units == 'Imperial') && ( weightType == 'Lbs') && ( heightType == 'Inches')) {
-              imperialBmi();
+        let impBmi = weight / height * 703;
+        if (units == 'Imperial' && weightType == 'Lbs' && heightType == 'Inches') {
+            imperialBmi();
+        };
             }
-    };
     let metricBmi = function(){
-            metBmi = weight / height;
-            if ( units == 'Metric') && (weightType == 'Kg') && (heightType == 'Cm')) {
-                metricBmi();
-            }
-        };  
+        let metBmi = weight / height;
+        if (units == 'Metric' && weightType == 'Kg' && heightType == 'Cm') {
+            metricBmi();
+        }
+    };
 });
